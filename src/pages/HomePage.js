1687d748ca93234b46e1584a8a6775e0b6a2b895
talhashakil155 <talhashakil155@gmail.com>
2024-10-26 14:51:05 +0500

    Meta adjustments

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
                 {/* Navigation */}
                 <nav className="flex items-center justify-between py-6">
                     <div className="text-2xl font-bold">Components Library</div>
-                    <div className="hidden md:flex items-center space-x-8">
+                    <div className="flex items-center space-x-8">
                         <a href="#" className="text-gray-700">Log in</a>
                         <button className="bg-purple-900 text-white px-4 py-2 rounded-lg">
                             Try now
@@ -18,16 +18,16 @@ const HomePage = () => {
 
                 <div className="max-w-7xl mx-auto pt-8 lg:pt-12 pb-12">
                     <h2 className='pt-8' style={{ fontSize: "18px" }}>Free React + Tailwind Components, Ready to Copy</h2>
-                    <div className='flex flex-wrap pt-14 space-x-4'>
+                    <div className='flex flex-wrap pt-14 gap-4'>
                         <Link to="/detail/hero-section-1" 
-                        className='w-full md:w-[45%] p-2 bg-white shadow-lg rounded-lg hover:bg-purple-300 focus:ring-1 focus:ring-opacity-50 transition duration-150 ease-in-out'>
+                        className='mb-4 w-full md:w-[45%] p-2 bg-white shadow-lg rounded-lg hover:bg-purple-300 focus:ring-1 focus:ring-opacity-50 transition duration-150 ease-in-out'>
                             <img
                                 src="hero-section-1.png"
                                 alt="hero section"
                             />
                         </Link>
                         <Link to="/detail/hero-section-2" 
-                        className='w-full md:w-[45%] p-2 bg-white shadow-lg rounded-lg hover:bg-purple-300 focus:ring-1 focus:ring-opacity-50 transition duration-150 ease-in-out'>
+                        className='mb-4  w-full md:w-[45%] p-2 bg-white shadow-lg rounded-lg hover:bg-purple-300 focus:ring-1 focus:ring-opacity-50 transition duration-150 ease-in-out'>
                             <img
                                 src="hero-section-2.png"
                                 alt="hero section"
